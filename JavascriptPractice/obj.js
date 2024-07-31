c2d9bd18fc218d041841461b148ba1e5cd1f4adf
khushbu-sahu <khushboosahu15@outlook.com>
2024-07-31 14:34:42 +0530

    practice

---
+++ JavascriptPractice/obj.js
@@ -0,0 +1,26 @@
+//OBJECTS IN JAVASCRIPT
+
+// const student={
+//     fullName:"hello javascript",
+//     age:21 ,
+//     cgpa:6.4,
+//     isPass:true
+// };
+
+// console.log(student);
+// console.log(student.fullName)
+// console.log(student["age"])
+// console.log(typeof student)
+// student["age"]=student["age"]+1;
+// console.log(student.age)
+
+
+//Practice Questions
+// const product={
+//     name:"Parker Jotter Standard CT Ball Pan(Black)",
+//     rating:4,
+//     price:270,
+//     offer:5,
+// };
+// console.log(product);
+
---
+++ JavascriptPractice/operator.js
@@ -0,0 +1,8 @@
+let a=5;
+let b=3;
+console.log ("a+b" , a+b)
+console.log ("a-b" , a-b)
+console.log ("a*b" , a*b)
+console.log ("a/b" , a/b)
+console.log ("a%b" , a%b)  //Modular operator
+console.log ("a**b" , a**b)  //Exponential operator 5^3(five to the power three)
